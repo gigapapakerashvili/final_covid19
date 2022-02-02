6ed80fc318329459af8c9bf034c71ff7c064321e
gigapapakerashvili <giorgi.papakerashvili.1@btu.edu.ge>
2022-02-02 11:03:07 +0400

    added migration and model, and relational structure.

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function statistics()
+    {
+    return $this->hasMany(CovidData::class, 'foreign_user_ID', 'id');
+    }
 }
